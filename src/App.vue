4167a7eecdcc1a1abb322e45e100d4babeebc07a
alessandroamormino <amormino.alessandro97@gmail.com>
2023-03-30 16:28:53 +0200

    feat: create func callAPI and pass string with store input

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import { store } from "./store.js";
 import axios from "axios";
 import AppHeader from "./components/AppHeader.vue";
 import AppMain from "./components/AppMain.vue";
+let requestAPI;
 export default {
   data() {
     return {
@@ -10,16 +11,26 @@ export default {
     };
   },
   created() {
-    let requestAPI = `${this.store.stringAPI}${this.store.trendingMovies}${this.store.key}`;
-    axios.get(requestAPI).then((res) => {
-      this.store.movies = res.data.results;
-    }).catch((error) => {
-      alert(`Error: ${error.response.status}`);
-    });
+    requestAPI = `${this.store.stringAPI}${this.store.trendingMovies}${this.store.key}`;
+    this.callAPI(requestAPI);
   },
   methods: {
+    callAPI(string) {
+      axios.get(string).then((res) => {
+        this.store.movies = res.data.results;
+      }).catch((error) => {
+        alert(`Error: ${error.response.status}`);
+      });
+    },
     searchMovie() {
       console.log(this.store.searchText);
+      if (this.store.searchText != '') {
+        requestAPI = `${this.store.stringAPI}${this.store.movieStringAPI}${this.store.key}&language=it&query=${this.store.searchText}`;
+        this.callAPI(requestAPI);
+      } else {
+        requestAPI = `${this.store.stringAPI}${this.store.trendingMovies}${this.store.key}`;
+        this.callAPI(requestAPI);
+      }
     }
   },
   components: {
