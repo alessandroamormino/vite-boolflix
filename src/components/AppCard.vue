04f91fd2cfe4646c9ff11b37b5bfad2b83bb144c
alessandroamormino <amormino.alessandro97@gmail.com>
2023-03-30 17:14:36 +0200

    feat: add AppCard component

---
+++ src/components/AppCard.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+  name: 'AppCard',
+  data() {
+    return {}
+  },
+  props: {
+    movies: Array,
+  },
+}
+</script>
+<template>
+  <div class="card" v-for="movie in movies">
+    <ul>
+      <li>Titolo: {{ movie.title }}</li>
+      <li v-if="movie.title != movie.original_title">
+        Titolo Originale: {{ movie.original_title }}
+      </li>
+      <li>Lingua: {{ movie.original_language }}</li>
+      <li>Voto: {{ movie.vote_average }}</li>
+    </ul>
+  </div>
+</template>
+<style lang="scss" scoped>
+.card {
+  border: 1px solid black;
+  margin-bottom: 1em;
+  // test
+}
+</style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,33 +1,20 @@
 <script>
 import { store } from "../store.js";
+import AppCard from "./AppCard.vue";
 export default {
-  name: 'AppMain',
+  name: "AppMain",
   data() {
     return {
       store,
-    }
-  }
+    };
+  },
+  components: { AppCard }
 }
 </script>
 <template>
   <div id="main-container">
     <h1>Movies_</h1>
-    <div class="card" v-for="movie in this.store.movies">
-      <ul>
-        <li>Titolo: {{ movie.title }}</li>
-        <li v-if="movie.title != movie.original_title">
-          Titolo Originale: {{ movie.original_title }}
-        </li>
-        <li>Lingua: {{ movie.original_language }}</li>
-        <li>Voto: {{ movie.vote_average }}</li>
-      </ul>
-    </div>
+    <AppCard :movies="this.store.movies"></AppCard>
   </div>
 </template>
-<style lang="scss" scoped>
-.card {
-  border: 1px solid black;
-  margin-bottom: 1em;
-  // test
-}
-</style>
+<style lang="scss" scoped></style>
